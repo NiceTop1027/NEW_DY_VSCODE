0549a5ce8f1f4ba32c5013a7b73c2beb854d446c
심우철 <nicetop@sim-ucheol-ui-MacBookPro.local>
2025-10-12 00:08:40 +0900

    🗑️ Remove Electron - Web-only version

--- electron-preload.js
+++
@@ -1,8 +0,0 @@
-// Electron Preload Script
-const { contextBridge } = require('electron');
-
-// Expose protected methods to renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-    isElectron: true,
-    platform: process.platform
-});
